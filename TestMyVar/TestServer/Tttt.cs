c46b1d0ab53f04f9929656857bf30b04fa4f6010
sww <swwefj@qq.com>
2014-08-11 07:26:31 +0000

    Add Server and Service

---
+++ TestMyVar/TestServer/Tttt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SVar.Codes;
+
+namespace TestServer
+{
+    public class Tttt:ITest
+    {
+        public DVar GetAllCities()
+        {
+            DVar re = new ArrayVar();
+            for (int i = 0; i < 10; i++) {
+                re[i] = new MapVar();
+                re[i]["id"] = i;
+                re[i]["name"] = "city" + i;
+            }
+            return re;
+        }
+    }
+}
